c24a61e103150c9608f48a318f4b0767b75e5445
buhrmi <buhrmi@gmail.com>
2016-05-03 16:46:52 -0700

    basic homepage

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -2,6 +2,7 @@ class Fund < ApplicationRecord
   belongs_to :user
   has_many :payments
 
-  Fund.where(:id => 1, :name => 'The Sound Check').first_or_create
 
+  first_user = User.where(:email => 'hello@newsoundforjapan.com').first_or_create(:password => 'hello', :password_confirmation => 'hello')
+  Fund.where(:id => 1, :name => 'The Sound Check', :user_id => first_user.id).first_or_create
 end
