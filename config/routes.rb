0543a0b5cfbdc0f0635216d895a0f76b0342a4af
buhrmi <buhrmi@gmail.com>
2016-08-01 11:15:52 -0700

    reboot

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :uploads
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root :to => 'funds#index'
+  
+  root :to => 'pages#index'
+  #root :to => 'funds#index'
 end
