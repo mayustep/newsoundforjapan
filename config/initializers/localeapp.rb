d74e439ce39763f2d8464d8d15355f3e567f7ae4
buhrmi <buhrmi@gmail.com>
2016-08-05 18:24:43 -0700

    poll translations in production

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,6 +1,11 @@
 require 'localeapp/rails'
 Localeapp.configure do |config|
   config.api_key = ENV['LOCALEAPP_KEY']
-  config.reloading_environments = [:development, :production, :staging]
-  config.polling_environments = []
+  config.reloading_environments = [:development, :production]
+  config.polling_environments = [:production]
+  config.poll_interval = 60
+end
+
+if Rails.env.production?
+  Localeapp::CLI::Pull.new.execute
 end
