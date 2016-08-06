8a3315f1857feccb2d125b1325823d88c9016de2
buhrmi <buhrmi@gmail.com>
2016-08-05 18:16:17 -0700

    reload translations in production too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :reload_localization
   before_action :use_user
   before_action :set_locale
   before_action :handle_secret
@@ -10,6 +11,11 @@ class ApplicationController < ActionController::Base
   around_action :atomic_posts
 
 private
+  def reload_localization
+    # currently on every request... whatever
+    I18n.backend.load_translations
+  end
+
   def use_user    
     @current_user = User.find(session[:user_id]) if session[:user_id] rescue nil
   end
